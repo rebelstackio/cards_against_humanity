b6dda0c7196b437774b8e85e9132f84ff444af1a
Javier Galarza <javier.galarza@dealerbuilt.com>
2020-10-01 20:46:59 -0500

    Fix auth lib on firebase backend

--- src/lib/backend/firebase/auth/index.js
+++ src/lib/backend/firebase/auth/index.js
@@ -1,25 +1,26 @@
 /* src/lib/backend/firebase/auth/index.js */
 
 /**
- *
- * @param {*} auth
+ * Sign in with google provider
+ * @param {object} auth Auth object
  */
 const singInWithGoogle = function _singInWithGoogle( auth = firebase.auth() ) {
-	return auth.singInWithGoogle;
+	const provider = new firebase.auth.GoogleAuthProvider();
+	return auth.signInWithPopup(provider);
 };
 
 /**
- *
- * @param {*} auth
+ * SignOut
+ * @param {object} auth Auth object
  */
 const signOut = function _signOut( auth = firebase.auth() ) {
 	return auth.signOut()
 };
 
 /**
- *
- * @param {*} auth
- * @param {*} next
+ * Event - Runs the next callbacks when the auth changes
+ * @param {function} next Event callback
+ * @param {object} auth Auth object
  */
 const onAuthStateChanged = function _onAuthStateChanged( next,  auth = firebase.auth()) {
 	auth.onAuthStateChanged(next);
