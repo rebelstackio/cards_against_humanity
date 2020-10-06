a1483a7f19162357bc821d4bdbe43395e09752d6
Javier Galarza <javier.galarza@dealerbuilt.com>
2020-10-06 16:46:32 -0500

    test on UI component

--- src/components/test/index.js
+++ src/components/test/index.js
@@ -1,5 +1,6 @@
 import { MetaComponent, Div } from '@rebelstack-io/metaflux';
 import DeckApi from '../../lib/backend/firebase/deck';
+import RoomApi from '../../lib/backend/firebase/room';
 
 class TestSuit extends MetaComponent {
 	constructor() {
@@ -7,13 +8,43 @@ class TestSuit extends MetaComponent {
 	}
 
 	render() {
-		DeckApi.getDecks().then((snapshot) => {
-			snapshot.forEach(function(doc) {
-				console.log(doc.id, " => ", doc.data());
-			});
-		}).catch((err) => {
-			console.error('error', err);
-		});
+		const props = {
+			name: 'testroom3',
+			size: 5,
+			deck: 1,
+			createdBy: 2,
+			nplayers: 2
+		};
+		// RoomApi.deleteRoom('Q3zb9DXBoCQkQfqW2VwU').then(() => {
+		// 	console.log('deleted room');
+		// }).catch((err) => {
+		// 	console.error('error', err);
+		// });
+
+		// RoomApi.createRoom(props).then((docRef) => {
+		// 	console.log('created room', docRef);
+		// }).catch((err) => {
+		// 	console.error('error', err);
+		// });
+
+		// RoomApi.listRooms().then((documentSnapshots) => {
+		// 	// console.log('list room', documentSnapshots.docs);
+		// 	documentSnapshots.docs.forEach(d => {
+		// 		console.log('room:', d.id, d.data())
+		// 	});
+		// }).catch((err) => {
+		// 	console.error('error', err);
+		// });
+
+		// DeckApi.getDecks().then((snapshot) => {
+		// 	snapshot.forEach(function(doc) {
+		// 		console.log(doc.id, " => ", doc.data());
+		// 	});
+		// }).catch((err) => {
+		// 	console.error('error', err);
+		// });
+
+
 		return Div();
 	}
 
