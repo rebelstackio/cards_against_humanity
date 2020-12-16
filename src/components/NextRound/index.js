28d0987c517ab3076d4f9f24797d9fea77a72753
Osmar Reyes <osmarreyesst@gmail.com>
2020-12-16 15:36:29 -0500

    add round winner big card to show, add next round timmer and ui indicator

--- src/components/NextRound/index.js
+++ src/components/NextRound/index.js
@@ -3,18 +3,31 @@ import HostApi from '../../lib/backend/firebase/host';
 
 const _storage = global.storage;
 
+const getCounter = function () {
+	let content = Span({}, '15s');
+		let interval = setInterval(() => {
+			let s = parseInt(content.innerText.replace(/s$/g, '')); s--;
+			content.innerText = s + 's';
+			if (s === 0) {
+				clearInterval(interval);
+				if (_storage.getState().Match.isHost) {
+					const { id, rounds, players, pool, winningScore } = _storage.getState().Match;
+					HostApi.NextRound(id, rounds, players, pool, winningScore)
+				}
+			}
+		}, 1000)
+		return content;
+}
+
 const NextRound = () => Div({className: 'next-turn away'},[
-	Span({}, 'Next Turn'),
-	Button({
-		className: 'btn black',
-		onclick: () => {
-			const { id, rounds, players, pool, winningScore } = _storage.getState().Match;
-			HostApi.NextRound(id, rounds, players, pool, winningScore)
-		}
-	}, 'GO!')
+	Span({}, 'Next Round strarts in: '),
+	Span({
+		id: 'counter'
+	}, '')
 ]).onStoreEvent('MATCH_UPDATE', (state, that) => {
-	if (_checkIsWinner() && state.Match.isHost) {
-		that.classList.remove('away')
+	if (_checkIsWinner()) {
+		that.classList.remove('away');
+		that.querySelector('#counter').append(getCounter());
 	} else {
 		that.classList.add('away');
 	}
